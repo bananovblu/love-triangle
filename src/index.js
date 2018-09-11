04a76c70426b7e2f3e5ad44ef94f5ca6821db0b7
Yauheni Baranau <barapcmax@gmail.com>
2018-09-11 20:48:45 +0300

    a

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+module.exports = function getLoveTrianglesCount(preferences = []) { 
+  let length = preferences.length; 
+  let count = 0; 
+  for (let i = 0; i<length; i++) { 
+  let lover = preferences[preferences[i]]; 
+  if (lover === i+1) { 
+  count++; 
+  } 
+  } 
+  if (count%2) { 
+  return 0; 
+  } 
+  return count/2; 
+  
+  };
