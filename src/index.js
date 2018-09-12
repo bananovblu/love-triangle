961aa586f8105a4a5df9cdf6acbe10d8d6c1cc02
Yauheni Baranau <barapcmax@gmail.com>
2018-09-12 10:43:36 +0300

    m

--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   let length = preferences.length; 
   let count = 0;  
   for (let i = 0; i<length; i++) { 
-    let lover = preferences[preferences[i]];
-    console.log(lover + '___' + preferences[i]) 
-    if (lover === i+1) {      
-      count++; 
+      let next = preferences[i] - 1;
+      if (next == i) continue;
+      let nextnext = preferences[next] - 1;
+      if (next == nextnext) continue;
+      let nextnextnext = preferences[nextnext] - 1;
+      if (nextnextnext == nextnext) continue;
+      if (nextnextnext == i) {
+          count++;
       }
-     }
-     if (count%2) { 
-       return 0; 
-      }
-       return count/2; 
+   }
+   return count / 3;
 };
